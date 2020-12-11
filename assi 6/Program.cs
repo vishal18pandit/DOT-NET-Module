14325612237807c54c262902acf7edc86efa0f5a
vishal18pandit <65332350+vishal18pandit@users.noreply.github.com>
2020-12-11 23:14:11 +0530

    Add files via upload

---
+++ assi 6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            demo d = new demo();
+            d.invalid += D_invalid;
+            d.Pa = 1000;
+            Console.WriteLine(d.Pa);
+            Console.ReadLine();
+        }
+        private static void D_invalid()
+        {
+            Console.WriteLine("Invalid value of a");
+        }
+    }
+
+    public delegate void InvalidPaHandler();
+    public class demo
+    {
+
+        public event InvalidPaHandler invalid;
+
+        private int a;
+        public int Pa
+        {
+            set
+            {
+                if (value < 100)
+                {
+                    a = value;
+                }
+                else
+                {
+                    invalid();
+                }
+            }
+            get
+            {
+                return a;
+            }
+        }
+    }
+}
---
+++ assi 7/Program (1).cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<decimal, decimal, decimal, decimal> q1 = (p, n, r) =>
+            {
+                return (p * n * r) / 100;
+            };
+
+            Console.WriteLine("SI is " + q1(10, 10, 2));
+
+            Func<int, int, bool> q2 = (a, b) =>
+            {
+                return a > b;
+            };
+
+            Console.WriteLine(q2(10, 20));
+
+            Predicate<int> q4 = x => x % 2 == 0;
+            Employee emp1 = new Employee();
+            emp1.Basic = 112;
+            Predicate<Employee> q5 = emp => emp1.Basic > 100;
+
+            Console.WriteLine(q5(emp1));
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        private int basic;
+        public int Basic
+        {
+            get
+            {
+                return basic;
+            }
+            set
+            {
+                if (value > 100)
+                {
+                    basic = value;
+                }
+            }
+        }
+    }
+}
